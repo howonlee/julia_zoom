3a3d37f0e27d6a98af59b2224d3cab664902243b
Howon Lee <howon@howonlee.com>
2015-08-15 12:35:35 -0700

    add the fractal

--- fractal.py
+++ fractal.py
@@ -1,28 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_vals():
+def julia_quadratic(c=complex(0, 0.65)):
     re_min, re_max = -2.0, 2.0
     im_min, im_max = -2.0, 2.0
-    real_range = np.arange(re_min, re_max, (re_max - re_min))
-    imag_range = np.arange(im_min, im_max, (im_max - im_min))
-    arr = something ############
-    return arr
-
-def julia_quadratic(arr, c=complex(0, 0.75)):
-    ##############3
-    new_arr = arr.copy()
-    for im in image_range:
-        for re in real_range:
+    w, h = 512, 512
+    real_range = np.arange(re_min, re_max, (re_max - re_min)/ w)
+    imag_range = np.arange(im_min, im_max, (im_max - im_min)/ h)
+    new_arr = np.zeros((len(imag_range), len(real_range)))
+    for im_idx, im in enumerate(imag_range):
+        for re_idx, re in enumerate(real_range):
             z = complex(re, im)
             n = 250
-            while abs(z) < 10 and n > 0:
+            while abs(z) < 10 and n > 50:
                 z = z * z + c
-                n -= 1
-            new_arr[imthing, rething] = n
+                n -= 5
+            new_arr[im_idx, re_idx] = n
     return new_arr
 
 if __name__ == "__main__":
-    arr = generate_vals()
-    arr = julia_quadratic(arr)
+    arr = julia_quadratic()
     plt.matshow(arr)
+    plt.show()
