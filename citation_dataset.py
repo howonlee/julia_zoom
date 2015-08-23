0356809805fbc69ae41dca9e9e3ba9abdc961b20
Howon Lee <howon@howonlee.com>
2015-08-23 09:45:39 -0700

    citation work

--- citation_dataset.py
+++ citation_dataset.py
@@ -1,7 +1,16 @@
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
+import scipy.sparse as sci_sp
 
 if __name__ == "__main__":
-    with open("/home/curuinor/data/Cit-HepPh.txt") as citation_file:
-        for line in citation_file:
-            print line
+    net = nx.read_edgelist("/home/curuinor/data/Cit-HepPh.txt")
+    net = net.subgraph(map(str, range(3000)))
+    print "net created"
+    net_arr = nx.to_numpy_matrix(net)
+    net_arr = np.squeeze(np.asarray(net_arr))
+    degrees = net_arr.sum(axis=0)
+    degrees = np.sort(degrees)
+    plt.plot(degrees)
+    plt.title("citation network degrees")
+    plt.savefig("./pics/citation_degrees")
