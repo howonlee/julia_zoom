573bbae09ae6aa3bf083a447f71e77b7b6de417f
Howon Lee <howon@howonlee.com>
2015-08-29 15:46:30 -0700

    writing without thinking

--- electrostatics.jl
+++ electrostatics.jl
@@ -2,10 +2,11 @@
 function electrostatics_energy(arr)
   arr_size = shape(arr)
   energy = 0
-  for i in arr_size[0]
+  for i in arr_size[0] #### range iterator or something
     for j in arr_size[1]
       for k in arr_size[0]
         for l in arr_size[1]
+          energy += something #######3
         end
       end
     end
@@ -17,10 +18,19 @@ function create_fractal()
   ####
 end
 
-function unscramble()
-  ####
+function scramble_fractal(arr)
+  scrambled
+end
+
+function unscramble(frac, num_iters)
+  unscrambled_frac = copy(frac)
+  for i in range(num_iters)
+    # do the optimization
+  end
+  unscrambled_frac
 end
 
 frac = create_fractal()
-unscrambled_frac = unscramble(frac)
+scrambled_fract = scramble_fractal(frac)
+unscrambled_frac = unscramble(scrambled_frac)
 # save the unscrambled_frac to file
