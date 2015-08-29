1a4c2867ae680093adef2b9f83e0f87dd1725feb
Howon Lee <howon@howonlee.com>
2015-08-29 15:44:21 -0700

    getting back into the mode of thinking I guess

--- electrostatics.jl
+++ electrostatics.jl
@@ -1,5 +1,16 @@
 
 function electrostatics_energy(arr)
+  arr_size = shape(arr)
+  energy = 0
+  for i in arr_size[0]
+    for j in arr_size[1]
+      for k in arr_size[0]
+        for l in arr_size[1]
+        end
+      end
+    end
+  end
+  energy
 end
 
 function create_fractal()
