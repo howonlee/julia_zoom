881de90c949a3e2ab2bbb950bb4a75bd523e1464
Howon Lee <howon@howonlee.com>
2015-08-29 15:40:50 -0700

    think up the energy for the electrostatics

---
+++ electrostatics.jl
@@ -0,0 +1,15 @@
+
+function electrostatics_energy(arr)
+end
+
+function create_fractal()
+  ####
+end
+
+function unscramble()
+  ####
+end
+
+frac = create_fractal()
+unscrambled_frac = unscramble(frac)
+# save the unscrambled_frac to file
